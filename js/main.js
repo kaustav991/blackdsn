78368c8d9ec67eb1bb40dbf01352965116674187
tushar <duttatushar51@gmail.com>
2023-03-20 15:44:30 +0530

    circular stylink fixing

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 /** @format */
 // swiper-sliders
-var swiper = new Swiper(".feed-slide", {
-  effect: "cards",
+var swiper = new Swiper('.feed-slide', {
+  effect: 'cards',
   loop: true,
   freeMode: true,
   parallax: true,
   speed: 1500,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
+    el: '.swiper-pagination',
+    type: 'bullets',
     dynamicBullets: true,
   },
 });
@@ -20,52 +20,34 @@ var swiper = new Swiper(".feed-slide", {
 // fractional - slide
 // ----------------------------------------
 
-var swiper = new Swiper(".post-slider", {
+var swiper = new Swiper('.post-slider', {
   slidesPerView: 1,
   centeredSlides: true,
   spaceBetween: 30,
   pagination: {
-    el: ".swip2",
-    type: "bullets",
+    el: '.swip2',
+    type: 'bullets',
     dynamicBullets: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
 
-// for locomotive scroll
-
-// var scroll = new LocomotiveScroll({
-//   el: document.querySelector('[data-scroll-container]'),
-//   smooth: true,
-// });
-
 // gsap styles
 gsap.registerPlugin(ScrollTrigger);
-let container = document.querySelector(".slide");
+let container = document.querySelector('.slide');
 // let sections = gsap.utils.toArray(".sec-item");
 
-// gsap.to(sections, {
-//   xPercent: -100 * (sections.length - 1),
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: ".slide",
-//     pin: true,
-//     scrub: 1,
-//     snap: 1 / (sections.length - 1),
-//     end: () => "+=" + document.querySelector(".slide").offsetWidth,
-//   },
-// });
 gsap.to(container, {
   x: () =>
-    -(container.scrollWidth - document.documentElement.clientWidth) + "px",
-  ease: "none",
+    -(container.scrollWidth - document.documentElement.clientWidth) + 'px',
+  ease: 'none',
   scrollTrigger: {
     trigger: container,
-    start: "top top",
-    end: () => "+=" + container.offsetWidth,
+    start: 'top top',
+    end: () => '+=' + container.offsetWidth,
     scrub: true,
     pin: true,
     anticipatePin: 1,
@@ -73,4 +55,3 @@ gsap.to(container, {
 });
 
 ScrollTrigger.refresh();
-
