2377bffcedc1a779f24ae3e9651ddb6f6ded34b6
kaustav991 <kaustav.sengupta@fortmindz.in>
2023-03-20 15:01:06 +0530

    fix:slider

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 /** @format */
 // swiper-sliders
-var swiper = new Swiper('.feed-slide', {
-  effect: 'cards',
+var swiper = new Swiper(".feed-slide", {
+  effect: "cards",
   loop: true,
   freeMode: true,
   parallax: true,
   speed: 1500,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
+    el: ".swiper-pagination",
+    type: "bullets",
     dynamicBullets: true,
   },
 });
@@ -20,18 +20,18 @@ var swiper = new Swiper('.feed-slide', {
 // fractional - slide
 // ----------------------------------------
 
-var swiper = new Swiper('.post-slider', {
+var swiper = new Swiper(".post-slider", {
   slidesPerView: 1,
   centeredSlides: true,
   spaceBetween: 30,
   pagination: {
-    el: '.swip2',
-    type: 'bullets',
+    el: ".swip2",
+    type: "bullets",
     dynamicBullets: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
@@ -44,17 +44,33 @@ var swiper = new Swiper('.post-slider', {
 
 // gsap styles
 gsap.registerPlugin(ScrollTrigger);
+let container = document.querySelector(".slide");
+// let sections = gsap.utils.toArray(".sec-item");
 
-let sections = gsap.utils.toArray('.sec-item');
-
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: 'none',
+// gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".slide",
+//     pin: true,
+//     scrub: 1,
+//     snap: 1 / (sections.length - 1),
+//     end: () => "+=" + document.querySelector(".slide").offsetWidth,
+//   },
+// });
+gsap.to(container, {
+  x: () =>
+    -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+  ease: "none",
   scrollTrigger: {
-    trigger: '.slide',
+    trigger: container,
+    start: "top top",
+    end: () => "+=" + container.offsetWidth,
+    scrub: true,
     pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    end: () => '+=' + document.querySelector('.slide').offsetWidth,
+    anticipatePin: 1,
   },
 });
+
+ScrollTrigger.refresh();
+
